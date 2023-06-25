d00ab642201d2027b1421fce3ce4751a03018bc0
Elian <eliangerardiso@gmail.com>
2023-06-25 16:19:14 -0600

    Added todoReducer for useTodo

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useReducer } from "react"
-import { todoReducer } from '../08-useReducer/todoReducer'
+import { todoReducer } from './todoReducer'
 const initialState = [
     // {
     //     id: new Date().getTime(),
